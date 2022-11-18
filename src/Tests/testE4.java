f79d972eec1a55136460a4a23d3b72f37c1bcf05
Yuuta094 <YvanSan094@gmail.com>
2022-11-18 13:49:37 +0100

    Ejercicio_4 Resuelto

--- src/Tests/testE4.java
+++ src/Tests/testE4.java
@@ -1,10 +1,45 @@
 package Tests;
 
+import java.util.List;
+
+import us.lsi.common.Files2;
+import us.lsi.tiposrecursivos.BinaryTree;
+import us.lsi.tiposrecursivos.Tree;
+
 public class testE4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Test_4_Binario();
+		Test_4_Nario();
+		}
+		
+		public static void  Test_4_Binario() {
+			String file = "datos/Ejercicio4DatosEntradaBinario.txt";
+			
+			
+			List<BinaryTree<String>> inputs = Files2
+					.streamFromFile(file)
+					.map(linea-> BinaryTree.parse(linea, s-> s.toString()))
+					.toList();
+			
+			for (BinaryTree<String>tree: inputs) {
+				Boolean res  = ejercicio4.ArbolBinarioE4.ejercicio4_Binario(tree);
+				System.out.println("Binario: " + tree + "\tresultado: " + res);
+			}
+		}
+		
+		public static void  Test_4_Nario() {
+			String file = "datos/Ejercicio4DatosEntradaNario.txt";
+			
+			
+			List<Tree<String>> inputs = Files2
+					.streamFromFile(file)
+					.map(linea-> Tree.parse(linea, s-> s.toString()))
+					.toList();
+			
+			for (Tree<String>tree: inputs) {
+				Boolean res  = ejercicio4.ArbolNarioE4.ejercicio4_Nario(tree);
+				System.out.println("N-Ario: " + tree + "\tresultado: " + res);
+			}
+		}
 	}
-
-}
--- src/ejercicio4/ArbolNarioE4.java
+++ src/ejercicio4/ArbolNarioE4.java
@@ -1,5 +1,59 @@
 package ejercicio4;
 
+import java.util.List;
+
+import us.lsi.tiposrecursivos.Tree;
+import us.lsi.tiposrecursivos.Tree.TEmpty;
+import us.lsi.tiposrecursivos.Tree.TLeaf;
+import us.lsi.tiposrecursivos.Tree.TNary;
+
 public class ArbolNarioE4 {
 
+	public static Boolean ejercicio4_Nario(Tree<String> tree) {
+		return ejercicio4_Nario(tree, false);
+	}
+
+	public static Boolean ejercicio4_Nario(Tree<String> tree, Boolean f) {
+
+		return switch (tree) {
+		case TEmpty<String> t -> f;
+		case TLeaf<String> t -> f;
+		case TNary<String> t -> {
+			String palabra = t.label().toString();       //la palabra a analizar
+			Integer SumVocales = vocales(palabra);		 //las vocales de esa palabra
+			Integer TotalVocales = 0;					 //Total de vocales
+			List<Tree<String>> child = t.elements();     //Hijos del arbol
+			
+			if (t.height() == 1) {						 //Si el arbol solo tiene raiz e hijos A(B,C)
+				for (Tree<String> raiz : t.elements()) {
+					SumVocales = vocales(palabra) + SumVocales;
+					TotalVocales = vocales(t.elements().toString());
+					if (TotalVocales == SumVocales) {
+						f = ejercicio4_Nario(raiz, true);
+					}
+				}
+			}
+			for (Tree<String> hijos : child) {           //En caso de que tenga raiz, nodo e hijos A(B(C,D),E(F,G))
+				SumVocales = vocales(palabra) + SumVocales;
+				TotalVocales = vocales(hijos.elements().toString());  
+				if (TotalVocales == SumVocales) {
+					f = ejercicio4_Nario(hijos, true);
+				}
+			}
+			yield f;
+		}
+
+		};
+	}
+
+	public static Integer vocales(String s) {
+		Integer n = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
+					|| s.charAt(i) == 'u') {
+				n++;
+			}
+		}
+		return n;
+	}
 }
